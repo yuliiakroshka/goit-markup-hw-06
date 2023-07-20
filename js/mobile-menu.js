13fdc9f804a711b7da327da832b8c2cb3a07ca51
yuliiakroshka <ulakroska8@gmail.com>
2023-07-20 12:18:52 +0200

    0

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -25,4 +25,8 @@
     openMenuBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
+
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  
 })();
